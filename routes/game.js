189870c493d9c54e30e66232acb0035932081668
bardybard <a2005magnus@gmail.com>
2022-11-23 20:19:52 +0100

    added scoring system

--- routes/game.js
+++ routes/game.js
@@ -4,8 +4,16 @@ const router = express.Router();
 router.get('/game/practice', (req, res) => {
     //retrieve classes from database and pass through as json
         res.render('practice', {
-            layout: 'game'
+            layout: 'game',
+            score: req.session.score,
+            bestscore: req.session.bestscore
         })
+});
+
+router.post('/game/practice', (req, res) => {
+    req.session.score = req.body.score
+    req.session.bestscore = req.body.bestscore;
+    res.redirect('#');
 });
 
 router.get('/game/test', (req, res) => {
